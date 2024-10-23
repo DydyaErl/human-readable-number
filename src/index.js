8aa3257a113fe91e0e7f69741d29268e5c4aae69
DydyaErl <soyadi6500@gmail.com>
2024-10-23 11:35:26 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   
-}
+    if (number === 0) return 'zero';
+  
+    let readableNumber = '';
+  
+    
+    if (number < 10) {
+      return units[number];
+    }
+  
+    
+    if (number >= 10 && number < 20) {
+      return teens[number - 10];
+    }
+  
+    if (number >= 20 && number < 100) {
+      readableNumber += tens[Math.floor(number / 10)];
+      if (number % 10 !== 0) {
+        readableNumber += ' ' + units[number % 10];
+      }
+      return readableNumber;
+    }
+  
+    
+    if (number >= 100 && number < 1000) {
+      readableNumber += units[Math.floor(number / 100)] + ' hundred';
+      if (number % 100 !== 0) {
+        readableNumber += ' ' + toReadable(number % 100); 
+      }
+      return readableNumber;
+    }
+  };
+  
